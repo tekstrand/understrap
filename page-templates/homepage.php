ec3213bb7b5fb70c2b4fa4a7adf928f8ac4febc0
Tom Ekstrand <thomas.ekstrand@bigcommerce.com>
2017-08-10 23:36:58 -0500

    Reorg

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -1,13 +1,12 @@
 <?php
 /**
- * The template for displaying all single posts.
+ * Template Name: Home page for one product
  *
  * @package understrap
  */
 
 get_header();
 $container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
 <div class="wrapper" id="single-wrapper">
